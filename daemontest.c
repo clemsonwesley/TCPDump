f0fa07f404bcc7a4b505369630edf5ae3b1318f0
cldombr <cldombr@g.clemson.edu>
2019-04-11 17:24:00 -0400

    Add files via upload

---
+++ daemontest.c
@@ -0,0 +1,56 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+
+
+int main(void){
+
+  pid_t pid, sid;
+
+  pid = fork();
+  if(pid < 0){
+    exit(EXIT_FAILURE);
+  }
+
+  if(pid > 0){
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  //open logs here
+  openlog("slog", LOG_PID|LOG_CONS, LOG_USER);
+
+  sid = setsid();
+  if(sid < 0){
+    exit(EXIT_FAILURE);
+  }
+
+  if((chdir("/")) < 0){
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  while(1){
+    //do something
+    syslog(LOG_INFO, "daemon log");
+    sleep(20);
+  }
+
+  exit(EXIT_SUCCESS);
+}
+
+
+
+
+
+
