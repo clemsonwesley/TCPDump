48208122249020d54f67ae0f32de589bc677b304
SolidOrange <wconcan@clemson.edu>
2019-04-29 02:10:40 -0400

    python file to check for intrusions and record the ip addresses and data in respective files

---
+++ flagData.py
@@ -0,0 +1,31 @@
+def main():
+    log_file = open('/etc/log', 'r')
+    trusted_ips = open('trustedIPs', 'r')
+    data_out = open('flaggedData.txt', 'w')
+    ip_out = open('flaggedIPs.txt', 'w')
+    flagged_ips = []
+    logging=False
+    # Get set of flagged IPs
+    for line in log_file:
+        line = line.split()
+        if (line[1].strip()=="IP"):
+            logging=False
+            bad_address = line[2].split('.')[0:-1]
+            address = '.'.join(bad_address)
+            if (address not in open('trustedIPs').read()):
+                data_out.write("-----------\n")
+                # If an IP address is flagged, get its info
+                data_info = "Address: " + address + " Flags: " + line[6] + " Length: " + line[-1] + "\n"
+                flagged_ips.append(address)
+                ip_out.write(address + "\n")
+                data_out.write(data_info)
+                logging=True
+        elif logging:
+            data_out.write(''.join(line) + "\n")
+
+    log_file.close()
+    trusted_ips.close()
+    data_out.close()
+    ip_out.close()
+
+main()
--- flagIPs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-grep IP /etc/log | awk {'print $3'} > tempSources.txt;
-sed 's/\.[^.]*$//' tempSources.txt > sources.txt;
-grep -v -f trustedIPs sources.txt > flaggedIPs.txt;
-rm tempSources.txt sources.txt;
