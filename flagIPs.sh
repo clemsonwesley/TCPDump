5558953b257a36d47446f30bab984b1f2af55fa6
SolidOrange <wconcan@clemson.edu>
2019-04-29 01:02:42 -0400

    added shell scripit to flag intruder ip addresses and put them in file

--- flagIPs.sh
+++ flagIPs.sh
@@ -2,3 +2,4 @@
 grep IP /etc/log | awk {'print $3'} > tempSources.txt;
 sed 's/\.[^.]*$//' tempSources.txt > sources.txt;
 grep -v -f trustedIPs sources.txt > flaggedIPs.txt;
+rm tempSources.txt sources.txt;
